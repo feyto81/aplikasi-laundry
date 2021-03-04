a7432bc0fcb4a866a0d7a50830ce2cc90dce0432
Feyto <feyto81@gmail.com>
2021-03-04 12:06:20 +0700

    add

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Outlet;
 use App\Models\Transaction;
+use App\Models\Member;
 use Illuminate\Http\Request;
 
 class TransactionController extends Controller
@@ -12,12 +14,15 @@ class TransactionController extends Controller
         $data['page_title'] = "Order List";
         $data['page_sub_title'] = "Order List";
         $data['transaction'] = Transaction::all();
+
         return view('transaction.index', $data);
     }
     public function create()
     {
         $data['page_title'] = "Transaction";
         $data['page_sub_title'] = "Add Transaction";
+        $data['outlet'] = Outlet::all();
+        $data['member'] = Member::all();
         return view('transaction.add', $data);
     }
 }
--- resources/views/transaction/add.blade.php
+++ resources/views/transaction/add.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.main')
 @section('title','Transaction | Laundry Application')
 @section('css')
-
+<link href="{{asset('assets/libs/datatables.net-bs4/css/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('assets/libs/datatables.net-buttons-bs4/css/buttons.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('assets/libs/datatables.net-responsive-bs4/css/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
+<link href="{{asset('assets/libs/sweetalert2/sweetalert2.min.css')}}" rel="stylesheet" type="text/css" />
 @endsection
 @section('content')
 <div class="page-content">
@@ -47,11 +50,126 @@
         </div>
         <br>
         <div class="row">
-            <form action="{{route('admin.cms_users.store')}}" method="POST" enctype="multipart/form-data">
-                
-            </form>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <table widht="100%">
+                            <tr>
+                                <tr>
+                                    <td style="vertical-align: top">
+                                        <label for="date">Date</label>
+                                    </td>
+                                    <td>
+                                        <div class="form-group">
+                                            <input type="text" id="date" name="date" readonly value="<?=date('Y-m-d H:i:s')?>" class="form-control">
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="vertical-align: top; width: 30%">
+                                        <label for="user">Officer</label>
+                                    </td>
+                                    <td>
+                                        <div class="form-group">
+                                            <input type="text" id="user_id" name="user_id" value="{{Auth::user()->name}}" class="form-control" readonly>
+                                        </div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td style="vertical-align: top">
+                                        <label for="member_id">Member</label>
+                                    </td>
+                                    <td>
+                                        <div class="form-group input-group">
+                                            <select class="form-select select2" id="level_id" name="member_id">
+                                                <option disabled selected>Select</option>
+                                                @foreach ($member as $item)
+                                                <option value="{{$item->id}}">{{$item->name}}</option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            </tr>
+                            
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <table widht="100%">
+                            <tr>
+                                
+                                <tr>
+                                    <td style="vertical-align: top">
+                                        <label for="barcode">Outlet</label>
+                                    </td>
+                                    <td>
+                                        <div class="form-group input-group">
+                                            <input type="hidden" id="outlet_id" name="outlet_id">
+                                            <input type="hidden" id="address" name="address">
+                                            <input type="hidden" id="phone_number" name="phone_number">
+                                            <input type="text" id="name" name="name" class="form-control" autofocus >
+                                            <span class="input-group-btn">
+                                                <button type="button" class="btn btn-info btn-flat" data-bs-toggle="modal" data-bs-target="#modal-outlet">
+                                                    <i class="fa fa-search"></i>
+                                                </button>
+                                            </span>
+                                        </div>
+                                    </td>
+                                </tr>
+
+                            </tr>
+                            
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
         
+    </div>
+</div>
+<div id="modal-outlet" class="modal fade" tabindex="-1" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title mt-0" id="myModalLabel">Outlet</h5>
+                <button type="button" id="closeModalOutlet" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body table-responsive">
+                <table id="datatable" class="table table-bordered dt-responsive  nowrap w-100">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Address</th>
+                      <th>Phone Number</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach($outlet as $data)
+                      <tr>
+                        <td>{{$data->name}}</td>
+                        <td>{{$data->address}}</td>
+                        <td>{{$data->phone_number}}</td>
+                        <td class="text-right">
+                          <button class="btn btn-info btn-xs" id="select"
+                           data-id="{{$data->id}}"
+                           data-name="{{$data->name}}"
+                           data-address="{{$data->address}}"
+                           data-phone_number="{{$data->phone_number}}">
+                            <i class="fa fa-check"></i> Select
+                          </button>
+                        </td>
+                      </tr>
+                    @endforeach
+                  </tbody>
+                </table>
+              </div>
+        </div>
     </div>
 </div>
 @endsection
@@ -64,4 +182,19 @@
 <script src="{{asset('assets/libs/bootstrap-maxlength/bootstrap-maxlength.min.js')}}"></script>
 <script src="{{asset('assets/libs/%40chenfengyuan/datepicker/datepicker.min.js')}}"></script>
 <script src="{{asset('assets/js/pages/form-advanced.init.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-bs4/js/dataTables.bootstrap4.min.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('assets/libs/datatables.net-responsive-bs4/js/responsive.bootstrap4.min.js')}}"></script>
+<script src="{{asset('assets/js/pages/datatables.init.js')}}"></script>  
+<script src="{{asset('assets/libs/sweetalert2/sweetalert2.min.js')}}"></script> 
+<script>
+$(document).on('click', '#select', function() {
+	$('#outlet_id').val($(this).data('id'))
+	$('#name').val($(this).data('name'))
+	$('#address').val($(this).data('address'))
+	$('#phone_number').val($(this).data('phone_number'))
+  $('#closeModalOutlet').click();
+});
+</script>
 @endpush
