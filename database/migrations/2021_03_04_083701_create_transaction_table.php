e39380ef2aebb59a666ebdc84a3fb678138e1421
Feyto <feyto81@gmail.com>
2021-03-04 08:47:14 +0700

    add

---
+++ database/migrations/2021_03_04_083701_create_transaction_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('outlet_id')->references('id')->on('outlet');
+            $table->unsignedBigInteger('outlet_id');
+            $table->foreign('member_id')->references('id')->on('member');
+            $table->unsignedBigInteger('member_id');
+            $table->dateTime('date');
+            $table->dateTime('pay_date');
+            $table->integer('additional cost');
+            $table->integer('discon');
+            $table->integer('tax');
+            $table->enum('status', ['baru', 'proses', 'selesai', 'diambil']);
+            $table->enum('paid', ['dibayar', 'belum_dibayar']);
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction');
+    }
+}
---
+++ database/migrations/2021_03_04_084135_create_transaction_detail_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionDetailTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaction_detail', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('transaction_id')->references('id')->on('transaction');
+            $table->unsignedBigInteger('transaction_id');
+            $table->foreign('paket_id')->references('id')->on('paket');
+            $table->unsignedBigInteger('paket_id');
+            $table->integer('qty');
+            $table->text('keterangan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaction_detail');
+    }
+}
